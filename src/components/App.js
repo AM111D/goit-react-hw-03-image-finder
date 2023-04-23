6e2de200165e3e8d51b3a9e288e7165de5202abd
AM111D <Dimasssnike@icloud.com>
2023-04-22 17:13:19 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-  // state = {  }
+  state = { 
+    images: null
+   }
 
   componentDidMount() {
-    fetch('https://pixabay.com/api/?q=cat&page=1&key=25187003-ac92f0861cd819d45c4ecbcb8&image_type=photo&orientation=horizontal&per_page=12').then( res => res.json()).then(console.log)
+     fetch('https://pixabay.com/api/?q=cat&page=1&key=25187003-ac92f0861cd819d45c4ecbcb8&image_type=photo&orientation=horizontal&per_page=12')
+    .then( res => res.json())
+    .then(res => console.log(res.hits) )
+     
   }
 
   render() {
     return (
       <div>
-        <p>qwe</p>
+        <ul>
+          
+        </ul>
       </div>
     );
   }
